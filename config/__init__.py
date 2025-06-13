bf94f48f1e020106db41d6bd852d099b4afe6895
Adam Bin Azmi <92119571+adamazm@users.noreply.github.com>
2025-06-13 13:04:58 +0200

    Delete config directory
    revamp

--- config/__init__.py
+++
@@ -1,3 +0,0 @@
-from .models import *
-from .browser_config import *
-from .result_instructions import *
--- config/browser_config.py
+++
@@ -1,8 +0,0 @@
-from browser_use import BrowserConfig
-
-"""
-Browser configuration for browser-use to run headless or not.
-"""
-browser_config = BrowserConfig(
-    headless=False
-)
